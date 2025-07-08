bc15d61b019faf7d3b4400a1e2ba1c9627027115
JoanChrispin8 <chrispinnjoan@gmail.com>
2025-07-08 19:37:53 +0530

    Add files via upload

---
+++ basic coding/Class and object and constructor problem.py
@@ -0,0 +1,23 @@
+#get student name,mark,and total
+class student:
+    def __init__(self):
+        self.name = ""
+        self.marks = []
+        self.total = 0
+
+    def get_data(self):
+        self.name = input("Enter your Name:")
+        print(f"Enter Marks for {self.name}:")
+        for i in range(5):
+            marks = int(input(f"Subject {i+1} Mark: "))
+            self.marks.append(marks)
+            self.total = sum(self.marks)
+
+    def display(self):
+        print(f"\n Name:",self.name)
+        print(self.marks)
+        print(self.total)        
+
+s=student()
+s.get_data()
+s.display() 
